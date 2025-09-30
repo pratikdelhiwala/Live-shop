57270cf2d25816f1f7c0640dab5be6435abdd15f
pratikdelhiwala <150779958+pratikdelhiwala@users.noreply.github.com>
2025-09-30 16:14:20 -0400

    Add files via upload

---
+++ users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+router.post('/', async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.json(user);
+});
+
+module.exports = router;
